05b8072c39693cf8c2b530d70ba5355078300431
eneguE <bogdevich96@hotmail.com>
2017-03-25 18:15:15 +0300

    PointReader is finished

--- src/main/java/com/bogdevich/task1/util/PointReader.java
+++ src/main/java/com/bogdevich/task1/util/PointReader.java
@@ -6,7 +6,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,22 +14,25 @@ import java.util.regex.Pattern;
  */
 public class PointReader {
     private final static String PATH = "src/main/resources/input.txt";
-    private final static String STRING_PATTERN = "x=[0-9]+ +y=[0-9]+ +z=[0-9]";
+    private final static String LINE_REGEX = "x=[0-9]+ +y=[0-9]+ +z=[0-9]";
     private final static int POINTS = 3;
     public static Point[] extractPointParams(String path){
-        Pattern pattern = Pattern.compile(STRING_PATTERN);
+        Pattern pattern = Pattern.compile(LINE_REGEX);
         Matcher matcher;
-        Point[] points = new Point[3];
+        Point[] point = new Point[3];
+        ArrayList<Point> points = new ArrayList<>();
         try{
             ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get(path), Charset.forName("utf-8")));
             for (String line :
                     lines) {
                 matcher = pattern.matcher(line);
                 if (matcher.matches()){
-                    for (String param :
-                            line.split(" +")) {
-
-                    }
+                    String[] parsedNumbers = line.replaceAll("[xyz]=","").split(" +");
+                    points.add(new Point(
+                            Integer.parseInt(parsedNumbers[0]),
+                            Integer.parseInt(parsedNumbers[1]),
+                            Integer.parseInt(parsedNumbers[2])
+                    ));
                 }
             }
 
